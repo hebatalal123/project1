1fe261520b62e5b81e5ac6431ffdef3f82a794dc
RO <RO@DESKTOP-A5LC99D>
2026-07-12 21:26:05 +0300

    Add project files.

---
+++ project1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Estimate for carpet cleaning service");
+
+            Console.Write("Number of small carpets: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Number of large carpets: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Price per small carpet: {25:C}");
+            Console.WriteLine($"Price per large carpet: {35:C}");
+
+            int cost = (number1 * 25) + (number2 * 35);
+            Console.WriteLine($"Cost: {cost:C}");
+
+            double tax = cost * 0.06;
+            Console.WriteLine($"Tax: {tax:C}");
+
+            double total = cost + tax;
+            Console.WriteLine($"Total estimate: {total:C}");
+
+            Console.WriteLine("This estimate is valid for 30 days.");
+        }
+    }
+}
---
+++ search task/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace search_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1234567;
+            Console.WriteLine($"{num:N}");
+        }
+    }
+}
